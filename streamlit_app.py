518e183edb3fbff86e1b39b327099c0d663b5b55
sanjaykumar <77737624+sandytamilan@users.noreply.github.com>
2023-02-16 12:00:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError 
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.
-amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit') 
+
 streamlit.title('My Mom\'s New Healthy Diner') 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -14,6 +12,11 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-
+                                2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit') 
+streamlit.dataframe(my_fruit_list)                                
+                                
 #Pick list
 fruits_selected = streamlit.multiselect('Pick some fruits:',
  list(my_fruit_list.index), ['Avocado', 'Strawberries'])
