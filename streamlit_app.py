6e447b43f5b2e1c0e6c7df0db6d0638b2fc4e857
sanjaykumar <77737624+sandytamilan@users.noreply.github.com>
2023-02-16 12:11:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
    back_from_function = get_fruityvice_data(fruit_choice)
    streamlit.dataframe(back_from_function)
 except URLError as e:
-streamlit.error()
+ streamlit.error()
 streamlit.header("View Our Fruit List -  Add Your Favourites!")
 #Snowflake related functions
 def get_fruit_load_list():
